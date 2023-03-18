866b1ce23a4bcd1cf77fbafec419cd5ea6f0a5c5
TLSlov <t.pokandekl@gmail.com>
2023-03-18 19:12:41 +0100

    Needs work on

--- rps.js
+++ rps.js
@@ -1,23 +1,57 @@
+let score = 0
+let choices = ['rock', 'paper', 'scissors'];
+
 function getComputerChoice() {   
-    let choices = ['rock', 'paper', 'scissors'];
     return choices[Math.floor(Math.random() * choices.length)];
+}
+
+function getPlayerChoice() {
+    let valid = false;
+    while (valid == false){
+        playerSelection = prompt("Rock, paper or scissors?");
+        if (playerSelection == null) {
+            continue;
+        }
+        playerSelection = playerSelection.toLowerCase();
+        if (choices.includes(playerSelection)) {
+            valid = true;
+            return playerSelection;
+        }
+    }
 }
 
 function playRound (playerSelection, computerSelection) {
     if ((playerSelection == 'rock' && computerSelection == 'scissors') ||
     (playerSelection == 'scissors' && computerSelection == 'paper') ||
-    (playerSelection == 'paper' && computerSelection == 'rock'))
+    (playerSelection == 'paper' && computerSelection == 'rock')) {
+        score++;
         return "You win! " + playerSelection + " beats " + computerSelection;
+    }
     
     else if (playerSelection == computerSelection) {
         return "It's a tie.";
     }
 
-    else
+    else {
+        score--;
         return "You lose! " + computerSelection + " beats " + playerSelection;
+    }
 }
 
-const playerSelection = prompt();
-const computerSelection = getComputerChoice();
-console.log(computerSelection);
-console.log(playRound(playerSelection, computerSelection));
+function game() {
+    for (i = 0; i < 5; i++) {
+        let playerSelection = getPlayerChoice();
+        let computerSelection = getComputerChoice();
+        console.log(playRound(playerSelection, computerSelection));
+    }
+    console.log("Game over");
+    if (score > 0) {
+        return "Winner is the player!";
+    }
+    else if (score < 0) {
+        return "Winner is the computer";
+    }
+    else
+        return "There is no winner somehow...";
+}
+game();
